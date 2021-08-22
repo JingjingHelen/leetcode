f0dd65f4ee48a0f2137a8a14cdb44ae96b657f4e
JingjingHelen <312162874@qq.com>
2021-08-22 21:33:00 +0800

    update leetcode-simple(29)

--- trailingZeroes.js
+++ trailingZeroes.js
@@ -1,24 +1,17 @@
 //题目：给定一个整数 n，返回 n! 结果尾数中零的数量。
 
-//思路:先算出阶乘结果，将计算字符串中0的个数
+//思路:看了题解，0的产生源于2×5，只要阶乘里边有2、5这两个因数 就会产生1个0，但是2这个因数会比5多 因为偶数太多了 所以 我们只需要统计1~n有几个5因数，即 n/5 + n/5^2 + n/5^3......因此可以使用递归形式
+
 
 var trailingZeroes = function(n) {
-    let result = multRes(n);
-    result = "" + result;
-    // console.log(result)
-    return result.split('0').length - 1;
+    if(n === 0) {
+        return 0;
+    }else {
+        let num = parseInt(n/5);
+        let count = num + trailingZeroes(num);
+        return count;
+    }
 };
 
-function multRes(num) {
-    if(num === 0 || num === 1) {
-        return 1;
-    }else {
-        return num*multRes(num-1);
-    }
-}
-
-trailingZeroes(7)
-//result:2  ×
-
-
-//我写出的程序所得出的结果是2，而题目得出的是1，错误的原因是因为，题意理解不到位，题意要求的是求出阶乘结果后尾部0的个数，而不是所有0的个数。故：我的程序还需要在进行修改
+trailingZeroes(30)
+//result:7  
